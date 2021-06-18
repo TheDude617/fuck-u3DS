fd9af687bc53224ff1eee877975c5403a07eb604
Glazed_Belmont <glazedbelmont@nintendohomebrew.com>
2021-06-18 14:21:05 -0400

    fixing a bit of code

--- main.c
+++ main.c
@@ -9,9 +9,12 @@
 int main(int argc, char **argv)
 {
 	gfxInitDefault();
+        PrintConsole topScreen, bottomScreen;
 
 	//Initialize console on top screen. Using NULL as the second argument tells the console library to use the internal console structure as current one
-	consoleInit(GFX_TOP, NULL);
+	consoleInit(GFX_TOP, &topScreen);
+        // Refer to https://libctru.devkitpro.org/console_8h.html
+	//consoleInit(GFX_BOTTOM, &bottomScreen);
 
 	//Move the cursor to row 15 and column 19 and then prints "Hello World!"
 	//To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
@@ -19,6 +22,7 @@ int main(int argc, char **argv)
 	//The top screen has 30 rows and 50 columns
 	//The bottom screen has 30 rows and 40 columns
 	
+	consoleSelect(&topScreen);
 	printf("\x1b[30;16HPress A to say Fuck you, Press B to say Bitch, Press Y to say Shut up, Press X to Clear the Screen, Press Start to exit.");
 
 	// Main loop
@@ -31,41 +35,13 @@ int main(int argc, char **argv)
 		u32 kDown = hidKeysDown();
 		
 		if (kDown & KEY_A) printf("\x1b[16;20HFuckYou");
-		
-		// Flush and swap framebuffers
-		gfxFlushBuffers();
-		gfxSwapBuffers();
-
-		//Wait for VBlank
-		gspWaitForVBlank();
 
 		if (kDown & KEY_B) printf("\x1b[16;20HBitch");
-
-		// Flush and swap framebuffers
-		gfxFlushBuffers();
-		gfxSwapBuffers();
-
-		//Wait for VBlank
-		gspWaitForVBlank();
 		
 		if (kDown & KEY_Y) printf("\x1b[16;20HShutUp");
 		
-		// Flush and swap framebuffers
-		gfxFlushBuffers();
-		gfxSwapBuffers();
+		if (kDown & KEY_X) consoleClear(); // Clears the selected console
 
-		//Wait for VBlank
-		gspWaitForVBlank();
-		
-		if (kDown & KEY_X) printf("\x1b");
-		
-		// Flush and swap framebuffers
-		gfxFlushBuffers();
-		gfxSwapBuffers();
-
-		//Wait for VBlank
-		gspWaitForVBlank();
-		
 		if (kDown & KEY_START) break; //break in order to return to hbmenu
 		
 		// Flush and swap framebuffers
